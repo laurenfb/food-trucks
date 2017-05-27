845a27fa75ebc8294e5b2bb42be6e8e194d62d14
laurenfb <laurenfriesbrink@gmail.com>
2017-05-27 12:52:15 -0700

    [food-trucks] foodtruckfetcher class

--- show_open_food_trucks.py
+++ show_open_food_trucks.py
@@ -4,10 +4,17 @@
 # > pip install requests
 # Documentation for the requests library can be found here: http://docs.python-requests.org/en/master/
 
+import datetime
 import requests
 
-url = "http://data.sfgov.org/resource/bbb8-hzi6.json"
+class FoodTruckFetcher(object):
+    def __init__(self):
+        self.url = "http://data.sfgov.org/resource/bbb8-hzi6.json"
+        self.now = datetime.datetime.now()
 
-response = requests.get(url)
-if response.status_code == 200:
-    data = response.json()
+# response = requests.get(url)
+# if response.status_code == 200:
+#     data = response.json()
+
+if __name__ == "__main__":
+    FoodTruckFetcher()
